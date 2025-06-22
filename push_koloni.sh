70b480da68ff944aa20983d74e0767857874798b
KoloniBangkit2025 <ptteknorukunberdikari@gmail.com>
2025-06-23 01:43:45 +0700

    Update otomatis dari Termux

--- push_koloni.sh
+++ push_koloni.sh
@@ -1,13 +1,15 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cd ~/KoloniBangkit2025
 echo "Tes dari Termux $(date)" >> tes_otomatis.txt
+
 git init
 git branch -M main
 git remote remove origin 2>/dev/null
-git remote add origin git@github.com:KoloniBangkit2025/KoloniBangkit2025.git
+git remote add origin https://github.com/KoloniBangkit2025/KoloniBangkit2025.git
 git add .
 git commit -m "Update otomatis dari Termux"
 git push origin main
+
 
 
 
